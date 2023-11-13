b9f4aeb7de583684c308175258fafd9c12e388d6
MaxM1130 <mama7243@colorado.edu>
2023-11-12 17:27:06 -0700

    some animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,18 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D rb;
-    
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private float dirX = 0f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
@@ -25,8 +28,29 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, 14f);
         }
 
-         
-       
+        UpdateAnimation();
+
+
+
+
+    }
+
+    private void UpdateAnimation()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("Running", true);
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("Running", true);
+
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+
 
     }
     
